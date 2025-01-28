3f0b893b911533580f2519bf86ec4dd16aaad7e2
yashgondaliya36 <yashgondaliya36@gmail.com>
2025-01-28 15:08:14 +0530

    data ingestion done

---
+++ src/StockScout/components/data_ingestion.py
@@ -0,0 +1,36 @@
+from src.StockScout import logger
+from src.StockScout.config.configuration import DataIngestionConfig
+import yfinance as yf
+import os
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+
+    
+    def download_file(self):
+
+        # Download AAPL data 
+        logger.info(f"{self.config.ticker}'s data download started")
+        data = yf.download(self.config.ticker)
+        logger.info(f"{self.config.ticker}'s data downloaded")
+
+
+        data.columns = data.columns.droplevel(1)
+        data = data['Close']
+        # Set filename 
+        filename = 'data.csv'
+        filepath = os.path.join(self.config.local_data_file, filename)
+
+        # Check if file exists, if so delete it
+        if os.path.isfile(filepath):
+            os.remove(filepath)
+            
+        # Save downloaded data to csv    
+        data.to_csv(filepath)
+
+        logger.info(f"File downloaded and saved to: {filepath}")
+
+
+    
---
+++ src/StockScout/entity/config_entity.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    ticker: str
+    local_data_file: Path
+
